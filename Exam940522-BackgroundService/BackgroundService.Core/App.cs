620a8ebfeb34d44bfe1f57acca419c80a8e33f9f
EhsanErshadi <ershadi.ehsan@gmail.com>
2015-08-13 12:15:05 +0430

    Setup Core with main ViewModel

---
+++ Exam940522-BackgroundService/BackgroundService.Core/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cirrious.CrossCore.IoC;
+
+namespace BackgroundService.Core
+{
+    public class App : Cirrious.MvvmCross.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .StartingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<ViewModel.VmMain>();
+        }
+    }
+}
---
+++ Exam940522-BackgroundService/BackgroundService.Core/ViewModel/VmMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace BackgroundService.Core.ViewModel
+{
+    class VmMain : MvxViewModel
+    {
+        private string _hello = "Hello MvvmCross";
+        public string Hello
+        {
+            get { return _hello; }
+            set { _hello = value; RaisePropertyChanged(() => Hello); }
+        }
+    }
+}
